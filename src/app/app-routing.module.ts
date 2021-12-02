cbfcb043da9965c0a3c750db38bb6a45712f0664
Lídia Lopes <lidiajlopes@gmail.com>
2021-12-02 13:16:44 -0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'search', component: HeroSearchComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
